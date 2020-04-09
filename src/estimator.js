d6663bf6e33b078b8c4a3d7ebb2380292a90d5fa
echelonnought <50064240+echelonnought@users.noreply.github.com>
2020-04-09 16:51:50 +0100

    Removing trailing white spaces

--- src/estimator.js
+++ src/estimator.js
@@ -3,22 +3,24 @@ const covid19ImpactEstimator = (data) => {
   const time = data.timeToElapse;
   const impact = {};
   const severeImpact = {};
-  if ((data.periodType) === 'days') {
+  if (data.periodType === 'days') {
     factor = Math.floor(time / 3);
-  } else if ((data.periodType) === 'weeks') {
+  } else if (data.periodType === 'weeks') {
     factor = Math.floor((7 * time) / 3);
-  } else if ((data.periodType) === 'months') {
+  } else if (data.periodType === 'months') {
     factor = Math.floor((30 * time) / 3);
   }
-  impact.currentlyInfected = ((data.reportedCases) * 10);
-  severeImpact.currentlyInfected = ((data.reportedCases) * 50);
-  impact.infectionsByRequestedTime = (impact.currentlyInfected * (2 ** factor));
-  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * (2 ** factor));
-  severeImpact.severeCasesByRequestedTime = 
-  Math.floor((severeImpact.infectionsByRequestedTime) * (0.15));
-  const hospitalBedsAvailable = Math.floor((data.totalHospitalsBeds) * (0.35));
-  severeImpact.hospitalBedsByRequestedTime = 
-  (hospitalBedsAvailable - severeImpact.severeCasesByRequestedTime);
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** factor;
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * 2 ** factor;
+  severeImpact.severeCasesByRequestedTime = Math.floor(
+    severeImpact.infectionsByRequestedTime * 0.15
+  );
+  const hospitalBedsAvailable = Math.floor(data.totalHospitalsBeds * 0.35);
+  severeImpact.hospitalBedsByRequestedTime =
+    hospitalBedsAvailable - severeImpact.severeCasesByRequestedTime;
   const output = {
     data,
     impact,
