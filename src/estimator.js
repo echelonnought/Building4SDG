831bcfb31ff5a99e740bcaed771675e83d486f05
echelonnought <50064240+echelonnought@users.noreply.github.com>
2020-04-09 09:14:12 +0100

    testing the challenge again

--- src/estimator.js
+++ src/estimator.js
@@ -3,18 +3,17 @@ const covid19ImpactEstimator = (data) => {
   const time = data.timeToElapse;
   const impact = {};
   const severeImpact = {};
-  if (data.periodType === 'days') {
+  if ((data.periodType) === 'days') {
     factor = Math.floor(time / 3);
-  } else if (data.periodType === 'weeks') {
+  } else if ((data.periodType) === 'weeks') {
     factor = Math.floor((7 * time) / 3);
-  } else if (data.periodType === 'months') {
+  } else if ((data.periodType) === 'months') {
     factor = Math.floor((30 * time) / 3);
   }
-  impact.currentlyInfected = data.reportedCases * 10;
-  severeImpact.currentlyInfected = data.reportedCases * 50;
-  impact.infectionByRequestedTime = impact.currentlyInfected * 2 ** factor;
-  severeImpact.infectionByRequestedTime =
-    severeImpact.currentlyInfected * 2 ** factor;
+  impact.currentlyInfected = ((data.reportedCases) * 10);
+  severeImpact.currentlyInfected = ((data.reportedCases) * 50);
+  impact.infectionByRequestedTime = (impact.currentlyInfected * (2 ** factor));
+  severeImpact.infectionByRequestedTime = (severeImpact.currentlyInfected * (2 ** factor));
   const output = {
     data,
     impact,
