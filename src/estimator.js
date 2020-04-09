3ca548ec70f6bc8761cc27d3464c56146cba8192
echelonnought <50064240+echelonnought@users.noreply.github.com>
2020-04-09 16:07:47 +0100

    removing trailing white spaces

--- src/estimator.js
+++ src/estimator.js
@@ -3,8 +3,6 @@ const covid19ImpactEstimator = (data) => {
   const time = data.timeToElapse;
   const impact = {};
   const severeImpact = {};
- 
-
   if ((data.periodType) === 'days') {
     factor = Math.floor(time / 3);
   } else if ((data.periodType) === 'weeks') {
@@ -12,18 +10,13 @@ const covid19ImpactEstimator = (data) => {
   } else if ((data.periodType) === 'months') {
     factor = Math.floor((30 * time) / 3);
   }
-
   impact.currentlyInfected = ((data.reportedCases) * 10);
   severeImpact.currentlyInfected = ((data.reportedCases) * 50);
   impact.infectionsByRequestedTime = (impact.currentlyInfected * (2 ** factor));
-  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * (2 ** factor));
-
-  
+  severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * (2 ** factor))
   severeImpact.severeCasesByRequestedTime = Math.floor((severeImpact.infectionsByRequestedTime) * (.15));
-
   const hospitalBedsAvailable = Math.floor((data.totalHospitalsBeds) * (.35));
   severeImpact.hospitalBedsByRequestedTime = hospitalBedsAvailable - severeImpact.severeCasesByRequestedTime;
-
   const output = {
     data,
     impact,
