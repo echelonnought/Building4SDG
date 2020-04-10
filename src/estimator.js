e6bf5a553a06da55bb07c0b97e2ec63108a83fa7
echelonnought <50064240+echelonnought@users.noreply.github.com>
2020-04-10 22:04:40 +0100

    some more code

--- src/estimator.js
+++ src/estimator.js
@@ -36,8 +36,8 @@ const covid19ImpactEstimator = (data) => {
   );
   const convertDollarsForSevereImpact = severeImpact.infectionsByRequestedTime * getFlightUSD;
   const convertDollarsForImpact = impact.infectionsByRequestedTime * getFlightUSD;
-  severeImpact.dollarsInFlight = Math.trunc(convertDollarsForSevereImpact.toFixed(2));
-  impact.dollarsInFlight = Math.trunc(convertDollarsForImpact.toFixed(2));
+  severeImpact.dollarsInFlight = Math.trunc(convertDollarsForSevereImpact);
+  impact.dollarsInFlight = Math.trunc(convertDollarsForImpact);
   const output = {
     data,
     impact,
